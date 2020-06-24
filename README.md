71f0332a28c62583f0ce79e7129be31bc1239c63
GitHub-Measurement Computer 2 <pil9@pitt.edu>
2020-06-24 11:03:49 -0400

    Reorganize

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # HatDrivers
  
+All the drivers hatlab needs in order to run with instrumentserver
+
+For installing, use a developer pip install:
+```
+pip install --no-deps -e /folder/to/HatDrivers/
+```
+
---
+++ setup.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 24 10:42:04 2020
+
+@author: Hatlab_3
+"""
+
+
+from setuptools import setup
+
+setup(name='HatDrivers',
+      version='0.0.1',
+      description='TBD',
+      url='',
+      author='Ryan Kaufman',
+      author_email='rrk26@pitt.edu',
+      license='MIT',
+      packages=['hatdrivers'],
+      zip_safe=False)
