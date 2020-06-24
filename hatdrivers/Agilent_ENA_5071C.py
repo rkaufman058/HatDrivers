556d3fa209bfd3ab6075373d5dc016fc5f2e0882
Ryan Kaufman <rrk26@pitt.edu>
2020-06-24 15:39:51 -0400

    Keysight driver added

--- hatdrivers/Agilent_ENA_5071C.py
+++ hatdrivers/Agilent_ENA_5071C.py
@@ -4,12 +4,14 @@
 #average method by Erick Brindock  7/15/16
 #driver rewritten by Ryan Kaufman 06/11/20 for Qcodes
 
-from qcodes import VisaInstrument 
 import visa
 import types
 import logging
 import numpy as np
 import time
+from qcodes import (Instrument, VisaInstrument,
+                    ManualParameter, MultiParameter,
+                    validators as vals)
 #from pyvisa.visa_exceptions import VisaIOError
 #triggered=[False]*159 
 
@@ -36,68 +38,193 @@ class Agilent_ENA_5071C(VisaInstrument):
         super().__init__(name, address, terminator = '\n', **kwargs)
 
         # Add in parameters
+        self.add_parameter('rfout', 
+                           get_cmd = ':OUTP?',
+                           set_cmd = ':OUTP {}',
+                           vals = vals.Ints(0,1), 
+                           get_parser = int
+                           )
+        self.add_parameter('nfpts', 
+                           get_cmd = ':SENS1:SWE:POIN?', 
+                           set_cmd = ':SENS1:SWE:POIN {}', 
+                           vals = vals.Ints(1,1601), 
+                           get_parser = int
+                           )
+        self.add_parameter('ifbw', 
+                           get_cmd = ':SENS1:BWID?', 
+                           set_cmd = ':SENS1:BWID {}', 
+                           vals = vals.Numbers(), #TODO: get range
+                           get_parser = float)
         self.add_parameter('power', 
                            get_cmd = ":SOUR1:POW?", 
-                           set_cmd = ":SOUR1:POW {:f}", 
+                           set_cmd = ":SOUR1:POW {}", 
                            unit = 'dBm', 
-                           get_parser = float)
+                           get_parser = float,
+                           vals = vals.Numbers(-85, 10)
+                           )
         self.add_parameter('power_start',
                            get_cmd = ':SOUR1:POW:STAR?',
-                           set_cmd = ':SOUR1:POW:STAR {:s}',
+                           set_cmd = ':SOUR1:POW:STAR {}',
                            unit = 'dBm',
-                           get_parser = float
+                           get_parser = float, 
+                           vals = vals.Numbers(-85, 10)
                            )
         self.add_parameter('power_stop', 
                            get_cmd = ':SOUR:POW:STOP?', 
-                           set_cmd = ':SOUR1:POW:STOP {:s}', 
+                           set_cmd = ':SOUR1:POW:STOP {}', 
                            unit = 'dBm', 
-                           get_parser = float), 
+                           get_parser = float, 
+                           vals = vals.Numbers(-85, 10)), 
         self.add_parameter('averaging', 
                            get_cmd = ':SENS1:AVER?',
-                           set_cmd = ':SENS1:AVER {:s}')
+                           set_cmd = ':SENS1:AVER {}', 
+                           get_parser = int, 
+                           vals = vals.Ints(0,1)
+                           )
+        self.add_parameter('avgnum', 
+                           get_cmd = ':SENS1:AVER:COUN?', 
+                           set_cmd = ':SENS1:AVER:COUN {}', 
+                           vals = vals.Ints(1), 
+                           get_parser = int
+                           )
         self.add_parameter('phase_offset', 
                            get_cmd = ':CALC1:CORR:OFFS:PHAS?', 
-                           set_cmd = ':CALC1:CORR:OFFS:PHAS {:s}', 
-                           get_parser = float)
+                           set_cmd = ':CALC1:CORR:OFFS:PHAS {}', 
+                           get_parser = float, 
+                           vals = vals.Numbers())
         self.add_parameter('electrical_delay', 
                            get_cmd = 'CALC1:CORR:EDEL:TIME?', 
-                           set_cmd = 'CALC1:CORR:EDEL:TIME {:s}', 
+                           set_cmd = 'CALC1:CORR:EDEL:TIME {}', 
                            unit = 's',
-                           get_parser = float
+                           get_parser = float,
+                           vals = vals.Numbers()
                            )
         self.add_parameter('trigger_source', 
-                           get_cmd = 'TRIG:SOUR?', 
-                           set_cmd = 'TRIG:SOUR {:s}', 
-                           vals = ['INT', 'EXT', 'MAN', 'BUS']
+                            get_cmd = 'TRIG:SOUR?', 
+                            set_cmd = 'TRIG:SOUR {}', 
+                            vals = vals.Enum('INT', 'EXT', 'MAN', 'BUS')
+                            )
+        self.add_parameter('trform', 
+                            get_cmd = ':CALC1:FORM?', 
+                            set_cmd = ':CALC1:FORM {}', 
+                            vals = vals.Enum('PLOG', 'MLOG', 'PHAS', 
+                                             'GDEL', 'SLIN', 'SLOG', 
+                                             'SCOM', 'SMIT', 'SADM', 
+                                             'PLIN', 'POL', 'MLIN', 
+                                             'SWR', 'REAL', 'IMAG', 
+                                             'UPH', 'PPH')
+                            '''
+                            Set trace format. MLOGarithmic|PHASe|GDELay| SLINear|
+                            SLOGarithmic|SCOMplex|SMITh|SADMittance|PLINear|PLOGarithmic|
+                            POLar|MLINear|SWR|REAL| IMAGinary|UPHase|PPHase
+                            '''
+                            )
+        self.add_parameter('math', 
+                           get_cmd = ':CALC1:MATH:FUNC?', 
+                           set_cmd = ':CALC1:MATH:FUNC {}', 
+                           vals = vals.Enum('ADD', 'SUBT', 'DIV', 'MULT', 'NORM')
+                           )
+        self.add_parameter('sweep_type',
+                           get_cmd = ':SENS1:SWE:TYPE?', 
+                           set_cmd = ':SENS1:SWE:TYPE {}', 
+                           vals = vals.Enum('LIN', 'LOG', 'SEGM', 'POW')
                            )
         self.add_parameter('correction', 
                            get_cmd = ':SENS1:CORR:STAT?', 
-                           set_cmd = ':SENS1:CORR:STAT {%s}')
+                           set_cmd = ':SENS1:CORR:STAT {}', 
+                           get_parser = int)
         self.add_parameter('smoothing', 
                            get_cmd = ':CALC1:SMO:STAT?', 
-                           set_cmd = ':CALC1:SMO:STAT {:s}', 
+                           set_cmd = ':CALC1:SMO:STAT {}', 
                            get_parser = float 
                            )
         self.add_parameter('trace', 
                            set_cmd = None, 
                            get_cmd = self.gettrace)
-        
-        def gettrace(self):
-            '''
-            Gets amp/phase stimulus data, returns 2 arrays
-            
-            Input:
-                None
-            Output:
-                mags (dB) phases (rad)
-            '''
-            logging.info(__name__ + ' : get amp, phase stim data')
-            strdata= str(self._visainstrument.ask(':CALC:DATA:FDATa?'))
-            data= np.array(map(float,strdata.split(',')))
-            data=data.reshape((len(data)/2,2))
-            return data.transpose() # mags, phase
+        self.add_parameter('fdata', 
+                           set_cmd = None, 
+                           get_cmd = self.getfdata)
+        self.add_parameter('pdata', 
+                           set_cmd = None, 
+                           get_cmd = self.getpdata)
+        self.connect_message()
         
         
+####################### Custom Functions 
+    def gettrace(self):
+        '''
+        Gets amp/phase stimulus data, returns 2 arrays
+        
+        Input:
+            None
+        Output:
+            mags (dB) phases (rad)
+        '''
+        logging.info(__name__ + ' : get amp, phase stim data')
+        strdata= str(self.ask(':CALC:DATA:FDATa?'))
+        data= np.array(list(map(float,strdata.split(','))))
+        data=data.reshape((int(np.size(data)/2)),2)
+        return data.transpose()
+        
+    def getfdata(self):
+        '''
+        Gets freq stimulus data, returns array
+        
+        Input:
+            None
+        Output:
+            freqvalues array (Hz)
+        '''
+        logging.info(__name__ + ' : get f stim data')
+        strdata= str(self._visainstrument.ask(':SENS1:FREQ:DATA?'))
+        return np.array(map(float,strdata.split(',')))
+    def getpdata(self):
+        '''
+        Get the probe power sweep range
+        
+        Input: 
+            None
+        Output:
+            probe power range (numpy array)
+        '''
+        logging.debug(__name__ + ' : get the probe power sweep range')
+        return np.linspace(self.get_power_start(), self.get_power_stop(), 1601)
+        
+    def set_bundle(self, bundle):
+        '''
+        This lets you set values for the VNA all at once in a dictionary by using a key
+        '''
+        for key in bundle:
+            try: 
+                if type(bundle[key]) == str: 
+                    eval("self."+key+"(str('"+bundle[key]+"'))") 
+                else:
+                    eval("self."+key+"(str("+str(bundle[key])+"))") 
+            except AttributeError:
+                raise Exception("Key '" + str(key)+"' not a valid VNA setting, check your dictionary for typos compared to VNA settings in driver using VNA.get_all()")
+
+        
+    def get_bundle(self,bundle):
+        ''' 
+        This retrieves the current elements in the bundle dictionary, but JUST the keys that you give it. It also calls BS if you give it a garbage key.
+        '''
+        for key in bundle:
+            try: 
+                if type(bundle[key]) == str: 
+                    bundle[key] = eval("self."+key+"(str('"+bundle[key]+"'))") 
+                else:
+                    bundle[key] = eval("self."+key+"(str("+str(bundle[key])+"))") 
+            except AttributeError:
+                raise Exception("Key '" + str(key)+"' not a valid VNA setting, check your dictionary for typos compared to VNA settings in driver using VNA.get_all()")
+                
+        return bundle.copy() #the copy is because you want them to be seperate from the original dictionary. If it were C they would have different pointers
+      
+    def data_to_mem(self):        
+        '''
+        Calls for data to be stored in memory
+        '''
+        logging.debug(__name__+": data to mem called")
+        self.write(":CALC1:MATH:MEM")
         
         
         
---
+++ hatdrivers/Keysight_N5183B.py
@@ -0,0 +1,227 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 24 14:03:04 2020
+
+A driver to control the Keysight MXG Analog Signal Generator N5183B using QCodes
+
+#original driver by Erick Brindock for Qtlab
+
+@author: Hatlab - Ryan Kaufman
+
+"""
+import types
+import logging
+import numpy as np
+import time
+from qcodes import (Instrument, VisaInstrument,
+                    ManualParameter, MultiParameter,
+                    validators as vals)
+
+class Keysight_N5183B(VisaInstrument):
+    #startup, setting all of the parameters, confirming connection
+    def __init__(self, name, address, **kwargs): 
+        
+        logging.info(__name__ + ' : Initializing instrument Agilent_E5071C')
+        
+        super().__init__(name, address, terminator = '\n', **kwargs)
+        
+        #add all parameters from old driver
+        self.add_parameter('output_status', 
+                           get_cmd = 'OUTP?', 
+                           set_cmd = 'OUTP {}', 
+                           vals = vals.Ints(0,1), 
+                           get_parser = int
+                           )
+        self.add_parameter('frequency', 
+                           get_cmd = 'FREQ:CW?', 
+                           set_cmd = 'FREQ:CW {}', 
+                           vals = vals.Numbers(1, 20e9), 
+                           get_parser = float,
+                           unit = 'Hz'
+                           )
+        self.add_parameter('reference_source', 
+                           get_cmd = 'ROSC:SOUR?', 
+                           set_cmd = 'ROSC:SOUR {}', 
+                           vals = vals.Enum('INT','EXT'), 
+                           get_parser = str
+                           )
+        self.add_parameter('alc_auto', 
+                           get_cmd = 'POW:ATT:AUTO?', 
+                           set_cmd = 'POW:ATT:AUTO {}', 
+                           vals = vals.Ints(0,1), 
+                           get_parser = int
+                           )
+        self.add_parameter('phase_adjust', 
+                           get_cmd = 'PHAS:ADJ?', 
+                           set_cmd = 'PHAS:ADJ {}', 
+                           vals = vals.Numbers(), 
+                           get_parser =float, 
+                           unit = 'Hz'
+                           )
+        self.add_parameter('power', 
+                           get_cmd = 'POW?', 
+                           set_cmd = 'POW {}DB', 
+                           vals = vals.Numbers(-20, 20), 
+                           get_parser = float, 
+                           unit = 'dBm'
+                           )
+        self.add_parameter('power_mode', 
+                           get_cmd = 'POW:MODE?', 
+                           set_cmd = 'POW:MODE {}', 
+                           vals = vals.Enum('CW', 'FIX', 'LIST'), 
+                           get_parser = str
+                           )
+        self.add_parameter('power_start', 
+                           get_cmd = 'POW:STAR?', 
+                           set_cmd = 'POW:STAR {}DB', 
+                           vals = vals.Numbers(-20,20), 
+                           get_parser = float, 
+                           unit = 'dBm'
+                           )
+        self.add_parameter('power_stop',
+                           get_cmd = 'POW:STOP?', 
+                           set_cmd = 'POW:STOP {}DB', 
+                           vals = vals.Numbers(-20,20), 
+                           get_parser = float, 
+                           unit = 'dBm'
+                           )
+        self.add_parameter('mod_status',
+                           get_cmd = 'OUTP_MOD?', 
+                           set_cmd = 'OUTP:MOD {}', 
+                           vals = vals.Ints(0,1), 
+                           get_parser = int
+                           )
+        self.add_parameter('frequency_mode',
+                           get_cmd = 'FREQ:MODE?', 
+                           set_cmd = 'FREQ:MODE {}',
+                           vals = vals.Enum('CW', 'FIX', 'LIST'), 
+                           get_parser = str
+                           )
+        self.add_parameter('sweep_generation_type',
+                           get_cmd = 'SWE:GEN?', 
+                           set_cmd = 'SWE:GEN {}', 
+                           vals = vals.Enum('ANAL', 'STEP'), 
+                           #Analog and stepped, respectively...
+                           #theres no way the engineers weren't 
+                           #chuckling to themselves when they programmed this 
+                           get_parser = str
+                           )
+        self.add_parameter('frequency_start',
+                           get_cmd = 'FREQ:STAR?', 
+                           set_cmd = 'FREQ:STAR {}', 
+                           vals = vals.Numbers(1, 20e9), 
+                           get_parser = float,
+                           unit = 'Hz'
+                           )
+        self.add_parameter('frequency_stop',
+                           get_cmd = 'FREQ:STOP?', 
+                           set_cmd = 'FREQ:STOP {}', 
+                           vals = vals.Numbers(1, 20e9), 
+                           get_parser = float,
+                           unit = 'Hz'
+                           )
+        self.add_parameter('dwell_time',
+                           get_cmd = 'SWE:DWEL?', 
+                           set_cmd = 'SWE:DWEL {}', 
+                           vals = vals.Numbers(), 
+                           get_parser = float,
+                           unit = 's'
+                           )
+        self.add_parameter('sweep_spacing',
+                           get_cmd = 'SWE:SPAC?', 
+                           set_cmd = 'SWE:SPAC {}', 
+                           vals = vals.Enum('LIN','LOG'), 
+                           get_parser = str
+                           )
+        self.add_parameter('sweep_points',
+                           get_cmd = 'SWE:POIN?', 
+                           set_cmd = 'SWE:POIN {}', 
+                           vals = vals.Ints(1), 
+                           get_parser = int
+                           )
+        self.add_parameter('sweep_direction',
+                           get_cmd = 'LIST:DIR?', 
+                           set_cmd = 'LIST:DIR {}', 
+                           vals = vals.Enum('UP','DOWN'), 
+                           get_parser = str
+                           )
+        self.add_parameter('sweep_type',
+                           get_cmd = 'LIST:TYPE?', 
+                           set_cmd = 'LIST:TYPE {}', 
+                           vals = vals.Enum('LIST', 'STEP'), 
+                           get_parser = str
+                           )
+        self.add_parameter('sweep_step',
+                           get_cmd = 'SWE:STEP?', 
+                           set_cmd = 'SWE:STEP {}Hz', 
+                           vals = vals.Numbers(0), 
+                           get_parser = float, 
+                           unit = 'Hz'
+                            )
+        self.add_parameter('trigger_source',
+                           get_cmd = 'LIST:TRIG:SOUR?', 
+                           set_cmd = 'LIST:TRIG:SOUR {}', 
+                           vals = vals.Enum('BUS', 'IMM', 'EXT', 'INT', 'KEY', 'TIM'),
+                           # Sets the trigger source for a list or step sweep event
+                           # source (str) : BUS | IMMediate | EXTernal | INTernal | KEY | TIMer
+                           get_parser = str
+                           )
+        self.add_parameter('trigger_wait_time',
+                           get_cmd = 'TRIG:TIM?', 
+                           set_cmd = 'TRIG:TIM {}s', 
+                           vals = vals.Numbers(0), 
+                           get_parser = float, 
+                           unit = 's'
+                           )
+        self.add_parameter('retrace', 
+                           get_cmd = 'LIST:RETR?', 
+                           set_cmd = 'LIST:RETR {}', 
+                           vals = vals.Ints(0,1), 
+                           get_parser = int
+                          )
+        self.connect_message()
+
+#Begin the custom functions, these are pretty much copied from Erick's code:
+    def send_instruction(self, instruction):
+        '''
+        Sends a command to the instrument
+            Input:
+                command (string) : command to be sent (see manual for commands)
+        '''
+        self._visainstrument.write(instruction)
+    def retrieve_data(self, query):
+        '''
+        Reads data from the instrument
+            Input:
+                query (string) : command to be sent (see manual for commands)
+            Output:
+                varies depending on command sent
+        '''
+        return self._visainstrument.ask(query)
+    def reset(self):
+        '''
+        Reset to default state
+        '''
+        self._visainstrument.write('*RST')
+    def get_all(self):
+
+        self.output_status()
+        self.frequency()
+        self.reference_source()
+        self.alc_auto()
+        self.phase_adjust()
+        self.power()
+        self.power_mode()
+        self.power_start()
+        self.power_stop()
+        self.mod_status()
+        self.frequency_mode()
+        self.sweep_type()
+        self.frequency_start()
+        self.frequency_stop()
+        self.dwell_time()
+        self.sweep_spacing()
+        self.sweep_points()
+        self.sweep_direction()
+        self.sweep_type()
+        self.sweep_step()
